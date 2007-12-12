04da546d2eec4c87e1b398fb04c79a5d64b4a422
inthrax <inthrax@18fc03b3-f040-0410-a858-09f3dfd7e8aa>
2007-12-12 16:30:46 +0000

    required by "debugging" tests

---
+++ t/TieStderr.pm
@@ -0,0 +1,24 @@
+package t::TieStderr;
+
+use strict;
+use base qw(Tie::Handle);
+
+sub TIEHANDLE {
+	my $pkg = shift;
+	my $x = '';
+	my $y = \$x; #'
+	return bless $y,$pkg;
+}
+
+sub PRINT {
+	my $self = shift;
+	$$self .= shift;
+}
+sub READLINE {
+	my $self = shift;
+	( my $var = $$self ) =~ s/\n$//;
+	$$self='';
+	return $var;
+}
+
+1;
