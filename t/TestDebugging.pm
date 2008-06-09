1fef1ef87e895d0fe33a0145f46809301ba466db
inthrax <inthrax@18fc03b3-f040-0410-a858-09f3dfd7e8aa>
2008-06-09 11:01:56 +0000

    add test module

---
+++ t/TestDebugging.pm
@@ -0,0 +1,41 @@
+package TestDebugging;
+
+use strict;
+use Sub::Uplevel;
+use Test::More();
+use Test::More::Warn;
+use t::TieStderr;
+
+use ex::provide
+	':all' => [qw(have_debug no_debug ok_debug)];
+
+sub have_debug (&$) {
+	my ($c,$n) = @_;
+	my $caller = caller;
+	my $t = qr/\[$caller:\d+:\] test/;
+	tie *STDERR,'t::TieStderr';
+	no_warn {
+		uplevel 2,$c,"test";
+	} 'warn '.$n;
+	my $val = <STDERR>;
+	untie *STDERR;
+	@_ = ($val,$t,$caller.' '.$n);
+	goto &Test::More::like;
+}
+
+*ok_debug = \*have_debug;
+
+sub no_debug (&$) {
+	my ($c,$n) = @_;
+	my $caller = caller;
+	tie *STDERR,'t::TieStderr';
+	no_warn {
+		uplevel 2,$c,"test";
+	} $caller.' warn '.$n;
+	my $val = <STDERR>;
+	untie *STDERR;
+	@_ = ($val,'',$caller.' '.$n);
+	goto &Test::More::is;
+}
+
+1;
