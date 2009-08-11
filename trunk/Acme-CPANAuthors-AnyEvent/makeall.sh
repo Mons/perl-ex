50b96ae3e875b8af72be697a9f0460631c0568ba
inthrax <inthrax@18fc03b3-f040-0410-a858-09f3dfd7e8aa>
2009-08-11 10:42:22 +0000

    subj

--- trunk/Acme-CPANAuthors-AnyEvent/makeall.sh
+++ trunk/Acme-CPANAuthors-AnyEvent/makeall.sh
@@ -11,11 +11,11 @@ $perl -i -lpne 's{^\s+$}{};s{^    ((?: {8})+)}{" "x(4+length($1)/2)}se;' README 
 $perl Makefile.PL && \
 rm *.tar.gz && \
 make manifest && \
-$perl -i -lne 'print unless /(?:\.tar\.gz$|^dist)/' MANIFEST && \
+$perl -i -lne 'print unless /(?:\.tar\.gz$|^dist|^tmp)/' MANIFEST && \
 make clean && \
 $perl Makefile.PL && \
 make && \
-make test && \
+TEST_AUTHOR=1 make test && \
 make disttest && \
 make dist && \
 cp -f *.tar.gz dist/ && \
